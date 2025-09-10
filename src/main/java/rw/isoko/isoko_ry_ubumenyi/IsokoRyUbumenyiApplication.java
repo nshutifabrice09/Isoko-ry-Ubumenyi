7f609cec6e99e9c6bb493b047d6eca2dff8663c1
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-10 22:47:37 +0200

    initial commit

---
+++ src/main/java/rw/isoko/isoko_ry_ubumenyi/IsokoRyUbumenyiApplication.java
@@ -0,0 +1,13 @@
+package rw.isoko.isoko_ry_ubumenyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsokoRyUbumenyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsokoRyUbumenyiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rw/isoko/isoko_ry_ubumenyi/IsokoRyUbumenyiApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.isoko.isoko_ry_ubumenyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsokoRyUbumenyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
