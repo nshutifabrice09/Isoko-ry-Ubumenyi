59c3fa5589b0ca69d68f878d326b2db871dc1e0d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-24 12:01:21 +0200

    adding constructor parameters commit

--- src/main/java/rw/isoko/isoko_ry_ubumenyi/service/EnrollmentServiceImplementation.java
+++ src/main/java/rw/isoko/isoko_ry_ubumenyi/service/EnrollmentServiceImplementation.java
@@ -1,13 +1,29 @@
 package rw.isoko.isoko_ry_ubumenyi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import rw.isoko.isoko_ry_ubumenyi.model.Enrollment;
+import rw.isoko.isoko_ry_ubumenyi.repository.CourseRepository;
+import rw.isoko.isoko_ry_ubumenyi.repository.EnrollmentRepository;
+import rw.isoko.isoko_ry_ubumenyi.repository.UserRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class EnrollmentServiceImplementation implements EnrollmentService{
+
+    private final EnrollmentRepository enrollmentRepository;
+    private final UserRepository userRepository;
+    private final CourseRepository courseRepository;
+
+    @Autowired
+    public EnrollmentServiceImplementation(EnrollmentRepository enrollmentRepository, UserRepository userRepository, CourseRepository courseRepository) {
+        this.enrollmentRepository = enrollmentRepository;
+        this.userRepository = userRepository;
+        this.courseRepository = courseRepository;
+    }
+
     @Override
     public Enrollment saveEnrollment(Enrollment enrollment, UUID userId, UUID courseId) {
         return null;
