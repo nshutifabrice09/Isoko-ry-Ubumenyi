0aaf434b4a67a9cf91facc57d8e306bbea1e3f3d
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-24 11:21:55 +0200

    adding constructor parameters commit

--- src/main/java/rw/isoko/isoko_ry_ubumenyi/service/AssignmentServiceImplementation.java
+++ src/main/java/rw/isoko/isoko_ry_ubumenyi/service/AssignmentServiceImplementation.java
@@ -2,12 +2,17 @@ package rw.isoko.isoko_ry_ubumenyi.service;
 
 import org.springframework.stereotype.Service;
 import rw.isoko.isoko_ry_ubumenyi.model.Assignment;
+import rw.isoko.isoko_ry_ubumenyi.repository.AssignmentRepository;
+import rw.isoko.isoko_ry_ubumenyi.repository.CourseRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class AssignmentServiceImplementation implements AssignmentService{
+
+    private final AssignmentRepository assignmentRepository;
+    private final CourseRepository courseRepository;
     @Override
     public Assignment saveAssignment(Assignment assignment, UUID courseId) {
         return null;
