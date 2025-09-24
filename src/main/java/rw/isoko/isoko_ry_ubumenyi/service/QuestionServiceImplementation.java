3d526a8752202b5812d80a541e17a16e9fafd9b5
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-24 13:31:59 +0200

    adding constructor parameters commit

--- src/main/java/rw/isoko/isoko_ry_ubumenyi/service/QuestionServiceImplementation.java
+++ src/main/java/rw/isoko/isoko_ry_ubumenyi/service/QuestionServiceImplementation.java
@@ -2,12 +2,22 @@ package rw.isoko.isoko_ry_ubumenyi.service;
 
 import org.springframework.stereotype.Service;
 import rw.isoko.isoko_ry_ubumenyi.model.Question;
+import rw.isoko.isoko_ry_ubumenyi.repository.QuestionRepository;
+import rw.isoko.isoko_ry_ubumenyi.repository.QuizRepository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 public class QuestionServiceImplementation implements QuestionService{
+    private final QuestionRepository questionRepository;
+    private final QuizRepository quizRepository;
+
+    public QuestionServiceImplementation(QuestionRepository questionRepository, QuizRepository quizRepository) {
+        this.questionRepository = questionRepository;
+        this.quizRepository = quizRepository;
+    }
+
     @Override
     public Question saveQuestion(Question question, UUID quizId) {
         return null;
