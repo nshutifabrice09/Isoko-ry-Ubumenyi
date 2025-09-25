95be8742ce7f7c6b57672e45654199cab6676159
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-09-25 20:08:10 +0200

    PostMapping and GetMapping API Test on AssignmentController

--- src/main/java/rw/isoko/isoko_ry_ubumenyi/controller/AssignmentController.java
+++ src/main/java/rw/isoko/isoko_ry_ubumenyi/controller/AssignmentController.java
@@ -1,9 +1,12 @@
 package rw.isoko.isoko_ry_ubumenyi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import rw.isoko.isoko_ry_ubumenyi.model.Assignment;
 import rw.isoko.isoko_ry_ubumenyi.service.AssignmentService;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -14,5 +17,15 @@ public class AssignmentController {
     @Autowired
     public AssignmentController(AssignmentService assignmentService) {
         this.assignmentService = assignmentService;
+    }
+
+    @PostMapping("/assignment/{courseId}")
+    public Assignment saveAssignment(@RequestBody Assignment assignment, @PathVariable ("courseId") UUID courseId){
+        return assignmentService.saveAssignment(assignment, courseId);
+    }
+
+    @GetMapping("/assignments")
+    public List<Assignment> assignmentList(){
+        return assignmentService.getAllAssignments();
     }
 }
